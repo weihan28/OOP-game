400bd83e43328f1345bfb3b49934c8bf46c7d71a
CaMo111 <109951019+CaMo111@users.noreply.github.com>
2024-05-03 09:50:59 +1000

    Initial update to new actors of req1

---
+++ src/game/actors/AlienBug.java
@@ -0,0 +1,7 @@
+package game.actors;
+
+public class AlienBug extends Monster {
+    public AlienBug(String name, char displayChar, int hitpoints) {
+        super(name, displayChar, hitpoints);
+    }
+}
---
+++ src/game/actors/SuspiciousAstronaut.java
@@ -0,0 +1,7 @@
+package game.actors;
+
+public class SuspiciousAstronaut extends Monster {
+    public SuspiciousAstronaut(String name, char displayChar, int hitpoints) {
+        super(name, displayChar, hitpoints);
+    }
+}
