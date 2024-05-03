b5509c098713962aa5b3e0c41e85ad6be63023be
Anthony Lew <alew0024@student.monash.edu>
2024-05-03 17:12:14 +1000

    Added SusAstronaut and Instant Kill behaviour

---
+++ src/game/actors/SuspiciousAstronaut.java
@@ -0,0 +1,35 @@
+package game.actors;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.behaviours.KillBehaviour;
+import game.behaviours.WanderBehaviour;
+
+public class SuspiciousAstronaut extends Monster{
+
+    /**
+     * A class representing the HuntsmanSpider monster.
+     */
+    public SuspiciousAstronaut() {
+        super("Suspicious Astronaut", 'ඞ', 99);
+        this.addCapability(Status.HOSTILE_TO_PLAYER);
+        // lower number keyed behaviours are prioritised first
+        this.behaviours.put(10, new KillBehaviour());
+        this.behaviours.put(20, new WanderBehaviour());
+    }
+
+    /**
+     * Creates and returns an intrinsic weapon.
+     * Override this method to create an intrinsic weapon for
+     * the current Actor with more interesting descriptions and/or different damage.
+     *
+     * @see Actor#getIntrinsicWeapon()
+     * @return a freshly-instantiated IntrinsicWeapon
+     */
+    @Override
+    public IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(1, "stabs", 100);
+    }
+}
