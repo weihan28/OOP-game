6ec5bab81343f8dec9aa19e871dd15008f5279f4
Anthony Lew <alew0024@student.monash.edu>
2024-05-05 11:10:05 +1000

    Changed FollowBehaviour to DoNothingAction when no valid moves found. (Stops actor from moving away from target)

--- src/game/behaviours/FollowBehaviour.java
+++ src/game/behaviours/FollowBehaviour.java
@@ -1,5 +1,6 @@
 package game.behaviours;
 
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.positions.Exit;
@@ -47,7 +48,7 @@ public class FollowBehaviour implements Behaviour {
 
     /**
      * Gets the next MoveActorAction to follow the target.
-     * Returns null if there are no valid directions that move the actor closer to the target.
+     * Returns doNothingAction if there are no valid directions that move the actor closer to the target.
      * @param actor
      * @param target
      * @param map
@@ -67,7 +68,7 @@ public class FollowBehaviour implements Behaviour {
                 }
             }
         }
-        return null;
+        return new DoNothingAction();
     }
 
     /**
