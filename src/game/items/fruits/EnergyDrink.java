96cb7d217217aef1ebb2f9f4464977069ddd058a
ncul0003 <ncul0003@student.monash.edu>
2024-05-07 16:25:48 +1000

    Energy drink functionality implemented

---
+++ src/game/items/fruits/EnergyDrink.java
@@ -0,0 +1,31 @@
+package game.items.fruits;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+
+public class EnergyDrink extends Item implements Consumable{
+    private final int healAmount = 1;
+    /***
+     * Constructor.
+     *  @param name the name of this Item
+     * @param displayChar the character to use to represent this item if it is on the ground
+     * @param portable true if and only if the Item can be picked up
+     */
+    public EnergyDrink(String name, char displayChar, boolean portable) {
+        super(name, displayChar, portable);
+    }
+
+    @Override
+    public String consumedBy(Actor actor) {
+        actor.heal(healAmount);
+        actor.removeItemFromInventory(this);
+        return String.format("%s heals %s by %d", this, actor,healAmount);
+    }
+    @Override
+    public ActionList allowableActions(Actor owner) {
+        ActionList actions = super.allowableActions(owner);
+        actions.add(new ConsumeAction(this));
+        return actions;
+    }
+}
