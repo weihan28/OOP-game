ad7f6da3417a5abaadb9c40b12ce72c4ccaf3653
alew0024 <alew0024@student.monash.edu>
2024-05-17 08:56:07 +1000

    Created AI Device and monologue classes.

---
+++ src/game/items/scraps/Monologuable.java
@@ -0,0 +1,16 @@
+package game.items.scraps;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+
+/**
+ * An interface for purchasable objects.
+ */
+public interface Monologuable {
+    /**
+     * Called when an actor talks to the object.
+     * @param actor The actor that is talking to the object.
+     * @return The description of the purchase result.
+     */
+    public ActionList getMonologue(Actor actor);
+}
