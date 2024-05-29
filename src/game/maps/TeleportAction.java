c4fec62b8943b4fcd95d38bb14fa82a41374ec3c
lcho0030 <lcho0030@student.monash.edu>
2024-05-30 08:48:05 +1000

    Created TeleportAction class

---
+++ src/game/maps/TeleportAction.java
@@ -0,0 +1,17 @@
+package game.maps;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class TeleportAction extends Action {
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
