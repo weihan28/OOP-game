69d3241d8cb6448178870fc728f571ccf2924878
lcho0030 <lcho0030@student.monash.edu>
2024-05-30 15:15:56 +1000

    Implemented the logic to get a random Location that is within the boundaries of the GameMap

--- src/game/maps/TeleportAction.java
+++ src/game/maps/TeleportAction.java
@@ -3,11 +3,24 @@ package game.maps;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+
+import java.util.Random;
 
 public class TeleportAction extends Action {
+
     @Override
     public String execute(Actor actor, GameMap map) {
-        return null;
+
+        Random random = new Random();
+        Location randomLocation;
+
+        int xCoordinate = random.nextInt(map.getXRange().max() + 1);  // max() returns the upper limit (inclusive), need to add 1 for bound
+        int yCoordinate = random.nextInt(map.getYRange().max() + 1);
+
+        randomLocation = new Location(map, xCoordinate, yCoordinate);
+
+
     }
 
     @Override
@@ -15,3 +28,8 @@ public class TeleportAction extends Action {
         return "Teleport with THESEUS";
     }
 }
+
+
+
+
+
