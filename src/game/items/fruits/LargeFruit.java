e1c261a8ffcfba1e5d5a9374b7b40f3de14bbb9d
Anthony Lew <ant87518@gmail.com>
2024-06-01 22:00:36 +1000

    Fixed large fuit selling

--- src/game/items/fruits/LargeFruit.java
+++ src/game/items/fruits/LargeFruit.java
@@ -1,7 +1,11 @@
 package game.items.fruits;
 
+import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.Status;
+import game.items.actions.SellAction;
 import game.items.actions.Sellable;
 
 /**
@@ -22,5 +26,14 @@ public class LargeFruit extends Fruit implements Sellable {
     @Override
     public int getSellValue() {
         return 30;
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, Location location){
+        ActionList actions = super.allowableActions(otherActor);
+        if (otherActor.hasCapability(Status.SELLABLE_TOWARDS)) {
+            actions.add(new SellAction(this));
+        }
+        return actions;
     }
 }
